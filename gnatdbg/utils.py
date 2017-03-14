a2d58f6b2132a81b923ecdc65c058f5c24960f90
Pierre-Marie de Rodat <derodat@adacore.com>
2017-03-14 15:02:13 +0100

    utils: move fat pointer processing to a new coerce_array function
    TN: O106-021

--- gnatdbg/utils.py
+++ gnatdbg/utils.py
@@ -35,40 +35,58 @@ def strip_type_name_suffix(type_name):
     return type_name
 
 
-def iter_array(array_value):
-    """Return an iterator that yields all elements in `array_value`."""
+def coerce_array(array_value):
+    """
+    Turn `array_value` into a proper GDB array value.
+
+    As long as we do not have DWARF/GDB proper support for unconstrained array,
+    we have to deal with fat pointers manually. If `array_type` is such a fat
+    pointer, create an array value with the appropriate bound info.
+
+    If `array_value` is already a ready-to-use array value, just return it.
+
+    If it's anything else, raise a ValueError.
+    """
     array_value = strip_typedefs(array_value)
 
-    # Note: as long as we do not have DWARF/GDB proper support for
-    # unconstrained array, we have to deal with fat pointers manually.
     if (
         array_value.type.code == gdb.TYPE_CODE_STRUCT
         and set(field.name for field in array_value.type.fields()) == {
             'P_ARRAY', 'P_BOUNDS'
         }
     ):
-        # Get the bounds.
+        # Get the bounds
         template = array_value['P_BOUNDS']
         first_index = int(template['LB0'])
         last_index = int(template['UB0'])
         if last_index < first_index:
             last_index = first_index - 1
 
-        # Then get and cast the array to the proper type.
+        # Then get and cast the array to the proper type
         array_ptr = array_value['P_ARRAY']
-        # Peel the pointer, typedef and array layer to rebuild an array type
-        # from the element type.
+
+        # Peel the pointer and array type layers
         array_ptr_type = array_ptr.type
         assert array_ptr_type.code == gdb.TYPE_CODE_PTR
         raw_array_type = array_ptr_type.target()
         assert raw_array_type.code == gdb.TYPE_CODE_ARRAY
         elt_type = raw_array_type.target()
 
+        # Rebuild an array type from the bounds + element type
         array_type = elt_type.array(first_index, last_index)
-        array_value = array_ptr.cast(array_type.pointer()).dereference()
+        return array_ptr.cast(array_type.pointer()).dereference()
 
     if array_value.type.code != gdb.TYPE_CODE_ARRAY:
-        raise ValueError('Invalid value type: was expecting an array')
+        raise ValueError('Invalid array value: {} ({})'.format(
+            array_value, gdb_code_names[array_value.type.code]
+        ))
+
+    return array_value
+
+
+def iter_array(array_value):
+    """Return an iterator that yields all elements in `array_value`."""
+    array_value = coerce_array(array_value)
 
     first_index, last_index = array_value.type.range()
     for i in range(first_index, last_index + 1):
