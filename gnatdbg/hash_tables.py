10a0462e6c2b990cfb61d9611e9fc140c65f8dca
Pierre-Marie de Rodat <derodat@adacore.com>
2017-03-14 15:32:19 +0100

    hash_tables: simplify iteration for null htable values
    TN: O106-021

--- gnatdbg/hash_tables.py
+++ gnatdbg/hash_tables.py
@@ -1,5 +1,5 @@
 from gnatdbg.generics import Match
-from gnatdbg.utils import iter_array
+from gnatdbg.utils import coerce_array, iter_array
 
 
 def get_htable_pattern(node_pattern):
@@ -26,15 +26,11 @@ def iterate(htable_value):
     Ada.Containers.Hash_Tables.Generic_Hash_Table_Types with Node_Type being a
     record that contains `element` and `next` fields.
     """
+    buckets = coerce_array(htable_value['buckets'])
+    if not buckets:
+        return
 
-    # TODO: For some reason, the access to unconstrained array in the `buckets`
-    # field does not work very well as a boolean. Revisit this once DWARF/GDB
-    # do things properly with respect to Ada types.
-    if str(htable_value['buckets']) != '0x0':
-        # Note: Due to a issue in DWARF/GDB, the `buckets` field appears here
-        # as a typedef to a fat pointer, not as a pointer (it is an access to
-        # unconstrained array in the sources.
-        for node in iter_array(htable_value['buckets']):
-            while node:
-                yield node
-                node = node['next']
+    for node in iter_array(buckets):
+        while node:
+            yield node
+            node = node['next']
