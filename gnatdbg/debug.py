513522a55adf1756d5fba6b7725b2d6904bec3b9
Pierre-Marie de Rodat <derodat@adacore.com>
2017-07-02 11:26:43 +0200

    Isolate the printing in PrintGDBTypeTreeCommand
    TN: Q106-021

--- gnatdbg/debug.py
+++ gnatdbg/debug.py
@@ -2,6 +2,54 @@ import itertools
 
 import gdb
 
+from gnatdbg.utils import gdb_code_names
+
+
+def print_type_tree(typeobj):
+    """
+    Print a GDB type as a tree on the standard output.
+
+    :param gdb.Type typeobj: Root type for the tree to print.
+    """
+    counter = iter(itertools.count(0))
+    visited = {}
+
+    def helper(t, indent=1):
+        indent_str = '  ' * indent
+        key = str(t)
+        try:
+            no = visited[key]
+        except KeyError:
+            no = next(counter)
+            visited[key] = no
+        else:
+            return '%{} ({})'.format(no, t.name)
+
+        result = '%{} ({}) = {}'.format(no, t.name,
+                                        gdb_code_names[t.code])
+        if t.code in (gdb.TYPE_CODE_PTR, gdb.TYPE_CODE_TYPEDEF):
+            result += ':\n{}{}'.format(
+                indent_str, helper(t.target(), indent + 1)
+            )
+        elif t.code == gdb.TYPE_CODE_INT:
+            result += ' ({} bytes)'.format(t.sizeof)
+        elif t.code == gdb.TYPE_CODE_ARRAY:
+            first, last = t.range()
+            result += '[{} .. {}]:\n{}{}'.format(
+                first, last,
+                indent_str, helper(t.target(), indent + 1)
+            )
+        elif t.code == gdb.TYPE_CODE_STRUCT:
+            result += ':'
+            for field in t.fields():
+                result += '\n{}{}: {}'.format(
+                    indent_str, field.name,
+                    helper(field.type, indent + 1)
+                )
+        return result
+
+    print(helper(typeobj))
+
 
 class PrintGDBTypeTreeCommand(gdb.Command):
     """
@@ -16,54 +64,7 @@ class PrintGDBTypeTreeCommand(gdb.Command):
             'dbgtype', gdb.COMMAND_NONE, gdb.COMPLETE_SYMBOL
         )
 
-        self.code_names = {
-            getattr(gdb, name): name
-            for name in dir(gdb)
-            if 'TYPE_CODE_' in name
-        }
-
     def invoke(self, arg, from_tty):
         argv = gdb.string_to_argv(arg)
-        value_type = gdb.parse_and_eval(argv[0]).type
-        self.print_type(value_type)
-
-    def print_type(self, value_type):
-        counter = iter(itertools.count(0))
-        visited = {}
-
-        def helper(t, indent=1):
-            indent_str = '  ' * indent
-            key = str(t)
-            try:
-                no = visited[key]
-            except KeyError:
-                no = next(counter)
-                visited[key] = no
-            else:
-                return '%{} ({})'.format(no, t.name)
-
-            result = '%{} ({}) = {}'.format(no, t.name,
-                                            self.code_names[t.code])
-            if t.code in (gdb.TYPE_CODE_PTR, gdb.TYPE_CODE_TYPEDEF):
-                result += ':\n{}{}'.format(
-                    indent_str, helper(t.target(), indent + 1)
-                )
-            elif t.code == gdb.TYPE_CODE_INT:
-                result += ' ({} bytes)'.format(t.sizeof)
-            elif t.code == gdb.TYPE_CODE_ARRAY:
-                first, last = t.range()
-                result += '[{} .. {}]:\n{}{}'.format(
-                    first, last,
-                    indent_str, helper(t.target(), indent + 1)
-                )
-            elif t.code == gdb.TYPE_CODE_STRUCT:
-                result += ':'
-                for field in t.fields():
-                    result += '\n{}{}: {}'.format(
-                        indent_str, field.name,
-                        helper(field.type, indent + 1)
-                    )
-            return result
-
-        gdb.write(helper(value_type))
-        gdb.write('\n')
+        typeobj = gdb.parse_and_eval(argv[0]).type
+        print_type_tree(typeobj)
