824269028b60bb6657e0aeccab8d53afcd98e2f5
Pierre-Marie de Rodat <derodat@adacore.com>
2018-12-13 10:18:56 +0100

    tests/print-gdb-type-tree: update baselines after recent changes
    Change-Id: Ieb91115b215259c24b910ca193c544f470ade656
    TN: RC11-021

--- testsuite/tests/print-gdb-type-tree/test.py
+++ testsuite/tests/print-gdb-type-tree/test.py
@@ -9,13 +9,13 @@ gdb.run_to(gdb.find_loc('foo.adb', 'BREAK'))
 gdb.execute('python import gnatdbg.debug')
 gdb.execute('python gnatdbg.debug.PrintGDBTypeTreeCommand()')
 
-gdb.test('dbgtype i', '%0 (integer) = TYPE_CODE_INT (4 bytes)')
+gdb.test('dbgtype i', '%0 (integer : TYPE_CODE_INT) (4 bytes)')
 
-gdb.test('dbgtype l', '%0 (foo__linked_list) = TYPE_CODE_TYPEDEF:'
-                      '  %1 (None) = TYPE_CODE_PTR:'
-                      '    %2 (foo__linked_list_record) = TYPE_CODE_STRUCT:'
-                      '      value: %3 (integer) = TYPE_CODE_INT (4 bytes)'
-                      '      next: %0 (foo__linked_list)')
+gdb.test('dbgtype l', '%0 (foo__linked_list : TYPE_CODE_TYPEDEF):'
+                      '  %1 (None : TYPE_CODE_PTR):'
+                      '    %2 (foo__linked_list_record : TYPE_CODE_STRUCT):'
+                      '      value: %3 (integer : TYPE_CODE_INT) (4 bytes)'
+                      '      next: %0 (foo__linked_list : TYPE_CODE_TYPEDEF)')
 
-gdb.test('dbgtype s', '%0 (foo__TsS) = TYPE_CODE_ARRAY[1 .. 13]:'
-                      '  %1 (character) = TYPE_CODE_CHAR')
+gdb.test('dbgtype s', '%0 (foo__TsS : TYPE_CODE_ARRAY)[1 .. 13]:'
+                      '  %1 (character : TYPE_CODE_CHAR)')
