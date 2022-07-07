91fee792af8c8fe068e153e7f7a9a3296ef07bef
Pierre-Marie de Rodat <derodat@adacore.com>
2022-07-07 11:32:14 +0000

    setup.py: transition to setuptools
    Also reformat it for black&flake8, and get the version number from the
    VERSION file.
    
    Change-Id: I5495a88f75bae224a21e4a125af4b4869b38727a
    TN: V707-013

--- setup.py
+++ setup.py
@@ -1,17 +1,24 @@
 #! /usr/bin/env python
 
-from distutils.core import setup
+import os.path
+
+from setuptools import setup
+
+
+# The actual version is stored in a separate file to ease automatic updates
+version_file = os.path.join(os.path.dirname(__file__), "VERSION")
+with open(version_file) as f:
+    version = f.read().strip()
 
 
 setup(
-    name='GNATdbg',
-    version='0.1-dev',
-    author='AdaCore',
-    author_email='report@adacore.com',
-    url='https://github.com/AdaCore/gnat-gdb-scripts',
-    description='Python helpers in GDB to deal with the GNAT runtime for Ada',
-    packages=['gnatdbg'],
-
+    name="GNATdbg",
+    version=version,
+    author="AdaCore",
+    author_email="report@adacore.com",
+    url="https://github.com/AdaCore/gnat-gdb-scripts",
+    description="Python helpers in GDB to deal with the GNAT runtime for Ada",
+    packages=["gnatdbg"],
     # This requires to be run in GDB's embedded Python interpreter
     requires=[],
 )
